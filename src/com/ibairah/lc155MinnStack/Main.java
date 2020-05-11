8a5a98dfcf6d4b6426f4a30bb65ac1f41e03cd26
ibairah2 <1557170743@qq.com>
2020-05-11 18:04:22 -0400

    lc 155 and 160

---
+++ src/com/ibairah/lc155MinnStack/Main.java
@@ -0,0 +1,136 @@
+package com.ibairah.lc155MinnStack;
+
+import java.util.Stack;
+
+/**
+ * Created by ibairah on 5/11/20.
+ *
+ * Design a stack that supports push, pop, top, and retrieving
+ * the minimum element in constant time.
+ *
+ * push(x) -- Push element x onto stack.
+ * pop() -- Removes the element on top of the stack.
+ * top() -- Get the top element.
+ * getMin() -- Retrieve the minimum element in the stack.
+ *
+ *
+ * Example 1:
+ *
+ * Input
+ * ["MinStack","push","push","push","getMin","pop","top","getMin"]
+ * [[],[-2],[0],[-3],[],[],[],[]]
+ *
+ * Output
+ * [null,null,null,null,-3,null,0,-2]
+ */
+public class Main {
+    public static void main(String[] args) {
+        MinStack minStack = new MinStack();
+        minStack.push(-2);
+        System.out.println(minStack.toString());
+        minStack.push(0);
+        System.out.println(minStack.toString());
+        minStack.push(-3);
+        System.out.println(minStack.toString());
+        minStack.getMin(); // return -3
+        System.out.println(minStack.getMin());
+        minStack.pop();
+        System.out.println(minStack.toString());
+        minStack.top();    // return 0
+        System.out.println(minStack.top());
+        minStack.getMin(); // return -2
+        System.out.println( minStack.getMin());
+
+        System.out.println( "----------------------------------------");
+
+        MinstackNode nstack = new MinstackNode();
+        nstack.push(-2);
+        System.out.println(nstack.toString());
+        nstack.push(0);
+        System.out.println(nstack.toString());
+        nstack.push(-3);
+        System.out.println(nstack.toString());
+        nstack.getMin(); // return -3
+        System.out.println(nstack.getMin());
+        nstack.pop();
+        System.out.println(nstack.toString());
+        nstack.top();    // return 0
+        System.out.println(nstack.top());
+        nstack.getMin(); // return -2
+        System.out.println( nstack.getMin());
+
+    }
+}
+
+
+class MinStack {
+    private Stack<Integer> stack = new Stack<>();
+    private Stack<Integer> minstack = new Stack<>();
+    /** initialize your data structure here. */
+    public MinStack() {
+    }
+    public void push(int x) {
+        stack.push(x);
+        // 如果最小stack为空，或者最小stack比x大，则push
+        if (minstack.isEmpty() || minstack.peek() >= x) {
+            minstack.push(x);
+        }
+    }
+    public void pop() {
+        // 如果当前被提出数值为最小数，则minstack同样也需要pop；
+        if(stack.peek().equals(minstack.peek())){
+            minstack.pop();
+        }
+        stack.pop();
+    }
+    public int top() {
+        return stack.peek();
+    }
+    public int getMin() {
+       return minstack.peek();
+    }
+}
+
+//  方法二 Node https://leetcode.com/problems/min-stack/discuss/49010/Clean-6ms-Java-solution
+
+class MinstackNode{
+    private class Node {
+        int val;
+        int min;
+        Node next;
+
+        // 创建两个node？
+        private Node(int val, int min) {
+            this(val, min, null);
+        }
+
+        // Node 里面有3个参数，（值，最小值，下一个Node地址）
+        private Node(int val, int min, Node next) {
+            this.val = val;
+            this.min = min;
+            this.next = next;
+        }
+    }
+
+    private Node head;
+
+   public void push(int x){
+       if(head == null){
+           head = new Node(x, x);
+       }else{
+           head = new Node(x, Math.min(x, head.min), head);
+       }
+   }
+
+   public void pop(){
+       head = head.next;
+   }
+
+   public int top(){
+       return head.val;
+   }
+
+   public int getMin(){
+       return head.min;
+   }
+}
---
+++ src/com/ibairah/lc160IntersectionOfTwoLinkedLists/main.java
@@ -0,0 +1,113 @@
+package com.ibairah.lc160IntersectionOfTwoLinkedLists;
+
+/**
+ * Created by ibairah on 5/11/20.
+ *
+ * Write a program to find the node at which the intersection of two singly linked lists begins.
+ *
+ * For example, the following two linked lists:
+ *
+ *    a1->a2->
+ *            c1->c2->c3
+ *b1->b2->b3->
+ * begin to intersect at node c1.
+ *
+ * Example 1:
+ *  4->1->
+ *          8->4->5
+ *5->0->1->
+ *
+ * Input: intersectVal = 8, listA = [4,1,8,4,5], listB = [5,0,1,8,4,5], skipA = 2, skipB = 3
+ * Output: Reference of the node with value = 8
+ * Input Explanation: The intersected node's value is 8 (note that this must not
+ * be 0 if the two lists intersect). From the head of A, it reads as [4,1,8,4,5].
+ * From the head of B, it reads as [5,0,1,8,4,5]. There are 2 nodes before the
+ * intersected node in A; There are 3 nodes before the intersected node in B.
+ *
+ *
+ * Example 2:
+ *
+ * 0->9->1->
+ *          2->4
+ *       3->
+ *
+ *
+ * Input: intersectVal = 2, listA = [0,9,1,2,4], listB = [3,2,4], skipA = 3, skipB = 1
+ * Output: Reference of the node with value = 2
+ * Input Explanation: The intersected node's value is 2 (note that this must not
+ * be 0 if the two lists intersect). From the head of A, it reads as [0,9,1,2,4].
+ * From the head of B, it reads as [3,2,4]. There are 3 nodes before the intersected
+ * node in A; There are 1 node before the intersected node in B.
+ *
+ *
+ * Example 3:
+ *2->6->4
+ *
+ *   1->5
+ *
+ * Input: intersectVal = 0, listA = [2,6,4], listB = [1,5], skipA = 3, skipB = 2
+ * Output: null
+ * Input Explanation: From the head of A, it reads as [2,6,4]. From the head of B,
+ * it reads as [1,5]. Since the two lists do not intersect, intersectVal must be 0,
+ * while skipA and skipB can be arbitrary values.
+ * Explanation: The two lists do not intersect, so return null.
+ *
+ *
+ * Notes:
+ *
+ * If the two linked lists have no intersection at all, return null.
+ * The linked lists must retain their original structure after the function returns.
+ * You may assume there are no cycles anywhere in the entire linked structure.
+ * Your code should preferably run in O(n) time and use only O(1) memory.
+ */
+
+
+public class main {
+    public static void main(String[] args) {
+
+    }
+}
+
+//    本题和141一个意思，都是找链表多久能相遇
+
+class ListNode{
+    int val;
+    ListNode next;
+    ListNode(int x){
+        val = x;
+        next = null;
+    }
+}
+
+class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        //boundary check
+        if (headA == null || headB == null) {
+            return null;
+        }
+
+        ListNode pointer1 = headA;
+        ListNode pointer2 = headB;
+
+        //if a & b have different len, then we will stop the loop after second iteration
+        while (pointer1 != pointer2) {
+            pointer1 = pointer1.next;
+            pointer2 = pointer2.next;
+
+            if (pointer1 == pointer2) {
+                return pointer1;
+            }
+
+            // pointer1 可以等于headA,但是runtime变成600ms，
+            // 而通过pointer1=headB，只需要1ms runtime
+            // ！！可能是因为为了寻找两个node 交互，则把指针放到相互的node上
+            if (pointer1 == null) {
+                pointer1 = headB;
+            }
+            if (pointer2 == null) {
+                pointer2 = headA;
+            }
+        }
+        return pointer1;
+    }
+}
